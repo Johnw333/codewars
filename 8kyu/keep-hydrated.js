982e570c9f14e7408e5fb5ac629d24beec3da945
John Wagenbrenner <johnwag333@gmail.com>
2024-03-19 13:03:57 -0400

    'updates'

--- 8kyu/keep-hydrated.js
+++ 8kyu/keep-hydrated.js
@@ -7,3 +7,11 @@
 function litres(time) {
   return Math.floor(time * 0.5);
 }
+
+// Alt Solutions:
+
+// function litres(time) {
+//   return Math.floor(time/2);
+// }
+
+// const litres = time => Math.floor(time * 0.5);
